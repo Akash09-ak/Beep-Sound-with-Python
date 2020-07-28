447c69101413faee0fa7db76e04074f0b5e7f764
Akash Kukreti <55475437+Akash09-ak@users.noreply.github.com>
2020-07-29 03:23:49 +0530

    Create voice to text.py

---
+++ voice to text.py
@@ -0,0 +1,6 @@
+import speech_recognition as sr
+r=sr.Recognizer()
+with sr.Microphone() as source:
+        print("Speak Anything: ")
+        audio=r.listen(source)
+        print(r.recognize_google(audio))
